4ea865c48fb805f7ea68e5643ef87dad5c78d848
Fadhlan Rizal <fadhlan.fari@gmail.com>
2022-08-07 19:28:09 +0700

    changing id to varchar for uuid

--- mysql/V1__init_table.sql
+++ mysql/V1__init_table.sql
@@ -1,7 +1,7 @@
 -- cool_database.companies definition
 
 CREATE TABLE `companies` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -12,7 +12,7 @@ CREATE TABLE `companies` (
 -- cool_database.notification_templates definition
 
 CREATE TABLE `notification_templates` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `notification_type` int(3) unsigned DEFAULT NULL,
   `action` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `subject` text COLLATE utf8mb4_unicode_ci,
@@ -27,7 +27,7 @@ CREATE TABLE `notification_templates` (
 -- cool_database.roles definition
 
 CREATE TABLE `roles` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role_type` int(3) unsigned DEFAULT NULL,
   `role_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` datetime NOT NULL,
@@ -47,15 +47,16 @@ CREATE TABLE `storages` (
   `original_filename` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `status` int(3) unsigned DEFAULT NULL,
   `created_by` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
 -- cool_database.transaction_items definition
 
 CREATE TABLE `transaction_items` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `transaction_id` bigint(20) unsigned DEFAULT NULL,
   `status` int(3) unsigned DEFAULT NULL,
   `created_by` bigint(20) unsigned DEFAULT NULL,
@@ -68,7 +69,7 @@ CREATE TABLE `transaction_items` (
 -- cool_database.transactions definition
 
 CREATE TABLE `transactions` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `transaction_no` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `status` int(3) unsigned DEFAULT NULL,
   `created_by` bigint(20) unsigned DEFAULT NULL,
@@ -81,7 +82,7 @@ CREATE TABLE `transactions` (
 -- cool_database.two_fa definition
 
 CREATE TABLE `two_fa` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` bigint(20) unsigned DEFAULT NULL,
   `account` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `issuer` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -96,7 +97,7 @@ CREATE TABLE `two_fa` (
 -- cool_database.two_fa_recoveries definition
 
 CREATE TABLE `two_fa_recoveries` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` bigint(20) unsigned DEFAULT NULL,
   `code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `status` int(3) unsigned DEFAULT NULL,
@@ -109,7 +110,7 @@ CREATE TABLE `two_fa_recoveries` (
 -- cool_database.user_roles definition
 
 CREATE TABLE `user_roles` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` bigint(20) unsigned DEFAULT NULL,
   `role_id` int(3) unsigned DEFAULT NULL,
   `created_at` datetime NOT NULL,
@@ -121,7 +122,7 @@ CREATE TABLE `user_roles` (
 -- cool_database.user_socials definition
 
 CREATE TABLE `user_socials` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` bigint(20) unsigned DEFAULT NULL,
   `notification_type` int(3) unsigned DEFAULT NULL,
   `token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -132,12 +133,12 @@ CREATE TABLE `user_socials` (
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
 -- cool_database.users definition
 
 CREATE TABLE `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -146,9 +147,11 @@ CREATE TABLE `users` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UN` (`email`),
+  UNIQUE KEY `username_UN` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
--- username/password : test/password
-INSERT INTO cool_database.users (username,password,email,status,mobile_phone,created_at,updated_at,deleted_at) VALUES
-	 ('test','$2a$15$2Jy6FN8YguSFSGaHQ7Tdm.hL3Lf/kMkjKDFDaC9WRBDtzZ0e6YRcu','test@gmail.com',99,'','2022-07-17 06:36:28','2022-07-17 06:36:28',NULL);
+-- username/email/password : test/test@gmail.com/password
+INSERT INTO cool_database.users(id, username, password, email, status, mobile_phone, created_at, updated_at, deleted_at)
+  VALUES('123e4567-e89b-12d3-a456-426614174000', 'test', '$2a$15$2Jy6FN8YguSFSGaHQ7Tdm.hL3Lf/kMkjKDFDaC9WRBDtzZ0e6YRcu', 'test@gmail.com', 99, '', '2022-07-17 06:36:28', '2022-07-17 06:36:28', NULL);
